4b4937033e267ec34d310ec18d591a6f88b0a698
ydz <67778879+yangdaozhao@users.noreply.github.com>
2022-09-17 08:15:29 +0800

    Update ylgy.js

--- ylgy.js
+++ ylgy.js
@@ -3,8 +3,8 @@ var url = $request.url
 
 if (body) {
   var obj = JSON.parse($response.body)
-  obj.data ='046ef1bab26e5b9bfe2473ded237b572'
-
+  obj.data.data ='046ef1bab26e5b9bfe2473ded237b572'
+  console.log(obj.data)
   $done({ body: JSON.stringify(obj) })
 } else {
   $done({})
