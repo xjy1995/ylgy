381f76ad82a1ea9d1a5f1d4a304ad6e39d04c84b
xjy1995 <xjy1995@users.noreply.github.com>
2022-09-27 09:46:59 +0800

    Update ylgy.js

--- ylgy.js
+++ ylgy.js
@@ -3,24 +3,7 @@ var url = $request.url
 
 if (body) {
   var obj = JSON.parse($response.body)
-  obj.blockTypeData = {
-    "1": 100,
-    "2": 0,
-    "3": 0,
-    "4": 0,
-    "5": 0,
-    "6": 0,
-    "7": 0,
-    "8": 0,
-    "9": 0,
-    "10": 0,
-    "11": 0,
-    "12": 0,
-    "13": 0,
-    "14": 0,
-    "15": 0,
-    "16": 0
-  }
+  obj.map_seed = [0, 0, 0, 0]
   $done({ body: JSON.stringify(obj) })
 } else {
   $done({})
