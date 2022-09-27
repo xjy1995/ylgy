5336373bb7f3d53ad15b7d8f57cc291f6a7bca00
xjy1995 <xjy1995@users.noreply.github.com>
2022-09-27 11:48:12 +0800

    Update ylgy.js

--- ylgy.js
+++ ylgy.js
@@ -3,7 +3,7 @@ var url = $request.url
 
 if (body) {
   var obj = JSON.parse($response.body)
-  obj.map_seed = [0, 0, 0, 0]
+  obj.data.map_seed = [0, 0, 0, 0]
   $done({ body: JSON.stringify(obj) })
 } else {
   $done({})
